29eb00c8a295df9c4c32708ee720e46faf673b48
engAbdullahHanif <engabdullahhanif@gmail.com>
2024-05-23 19:03:29 +0200

    session 10 added

--- 9 - session /stack-queue.py
+++ 9 - session /stack-queue.py
@@ -1,8 +1,72 @@
+
+
 # Problem: Valid Parentheses
 # Problem: Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid. An input string is valid if:
 
 # Open brackets must be closed by the same type of brackets.
 # Open brackets must be closed in the correct order.
+
+my_string = '()'
+my_string = '[]'
+my_string = '{]'
+
+
+my_string = '([)]'
+my_string = '{[]}'
+
+
+first_condition = '()'
+second_condition = '[]'
+third_condition = '{}'
+
+# if my_string == first_condition or my_string == second_condition or my_string == third_condition:
+#     print('True')
+# else:
+#     print('False')
+
+# my_string = '{]'
+# my_string = 'sdjf'
+# my_string = '){}[][]{[}]'
+
+# my_name = {'name': 'Hanif'}
+
+my_string = '()'
+my_string = ')('
+
+my_stack = []
+my_dict = {'(': ')', '[': ']', '{': '}'}
+
+for char in my_string:
+    if char in my_dict.keys():
+        print("opening bracket added")
+        my_stack.append(char)
+    elif char in my_dict.values():
+        if len(my_stack) == 0:
+            print('False, empty stack')
+            break
+        else:
+            last_open = my_stack.pop() # '('    
+            # if '('
+            if my_dict[last_open] == char:
+                print('True, it is valid')
+                break
+
+
+keys = ['(', '[', '{']
+values = [')', ']', '}']
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Stack
@@ -22,6 +86,7 @@
 # print('length of my_array: ', len(my_array))
 
 # stack.pop()
+
 
 
 
@@ -63,13 +128,13 @@ stack.push('a')
 stack.push('b')
 stack.push('c')
 
-print("stack.pop(): ", stack.pop())
-print("stack.peek(): ", stack.peek())
+# print("stack.pop(): ", stack.pop())
+# print("stack.peek(): ", stack.peek())
 
 # stack.pop()
 # stack.peek()
-print(stack.isEmpty())
-print(stack.size())
+# print(stack.isEmpty())
+# print(stack.size())
 
 
 
@@ -83,12 +148,32 @@ print(stack.size())
 # size
 
 class Queue():
-    # def dequeue(self):
-    #     if self.isEmpty():
-    #         return None
-    #     return self.queue.pop(0)
+    def dequeue(self):
+        if self.isEmpty():
+            return None
+        # return self.items[0].pop()
+        return self.items.pop(0)
+
     
     # def enqueue(self, item):
     #     self.queue.append(item)
 
+    # def dequeue(self):
+        #     if self.is_empty():
+
+        #         return "Q is empty"
+
+        #         return Self.queue.dequeue()
+
+    def dequeue(self):
+        if self.isEmpty():
+            return None
+        else:
+            # return self.queue.pop(0)
+            return self.queue[0].pop()
+
     pass
+
+my_queue = [1, 2, 3, 4, 5, 7]
+
+
