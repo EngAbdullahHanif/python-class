e99556e7e4cf6f103c8eab042641907e67d7dfdd
engAbdullahHanif <engabdullahhanif@gmail.com>
2024-06-12 21:02:00 +0200

    last session, completed

--- 10 - session/linked_list.py
+++ 10 - session/linked_list.py
@@ -79,10 +79,10 @@ class SinglyLinkedList:
 
 # Example usage:
 sll = SinglyLinkedList()
-sll.append(1)
-sll.append(2)
-sll.append(3)
-sll.print_list()  # Output: 1 -> 2 -> 3 -> None
+# sll.append(1)
+# sll.append(2)
+# sll.append(3)
+# sll.print_list()  # Output: 1 -> 2 -> 3 -> None
 
 
 
@@ -95,6 +95,14 @@ class PageNode:
         self.url = url 
         self.prev = None
         self.next = None
+
+# my_prev = 4
+# current = 5
+# my_next = 6
+
+# print(my_prev) # 4
+# print(current) # 5
+# print(current) # 6
        
 class BrowserHistory:
 
@@ -108,18 +116,28 @@ class BrowserHistory:
         if not self.head:
             self.head = new_page
             self.current = new_page
+            print(f"Visited: {self.current.url}")
             return
         
         self.current.next = new_page
         new_page.prev = self.current
         self.current = new_page
+        print(f"Visited: {self.current.url}")
             
 
     def back(self):
-        pass
+        if self.current.prev:
+            self.current = self.current.prev
+            print(f"Back to: {self.current.url}")
+        else:
+            print("No back history available")
 
     def forward(self):
-        pass
+        if self.current.next:
+            self.current = self.current.next
+            print(f"Forward to: {self.current.url}")
+        else:
+            print("No forward history available")
 
 # Example usage
 browser = BrowserHistory()
@@ -129,3 +147,25 @@ browser.visit("stackoverflow.com")
 browser.back()  # Output: Back to: github.com
 browser.back()  # Output: Back to: google.com
 browser.forward()  # Output: Forward to: github.com
+browser.forward()  # Output: Forward to: stackoverflow.com
+
+
+
+# x = 5 
+# my_array = [1, 2, 3, 4, 5]
+# my_object = {
+#     "name": "John",
+#     "age": 30
+# }
+
+# my_stack = []
+# my_stack.append(1)
+
+# my_queue = []
+# my_queue.append(1)
+
+# my_linkedlist = SinglyLinkedList()
+# my_linkedlist.append(1)
+
+# my_double_linkedlist = DoublyLinkedList()
+# my_double_linkedlist.append(1)
